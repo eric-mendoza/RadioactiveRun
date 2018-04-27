dfbd2cfb4219f8eb19cb3114bf9b7c07cec63aa5
Eric <eric.mendoza333@gmail.com>
2018-04-26 21:54:15 -0600

    Initial Commit

---
+++ Assets/Script/EscogerPersonaje.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EscogerPersonaje : MonoBehaviour {
+    public GameObject personajePrefab;
+    public GameObject personajeContainter;
+
+    private void Start() {
+        Sprite[] personajes = Resources.LoadAll<Sprite>("Personajes");
+        foreach (Sprite personaje in personajes) {
+            GameObject container = Instantiate(personajePrefab) as GameObject;
+            container.GetComponent<Image>().sprite = personaje;
+            container.transform.SetParent(personajeContainter.transform, false);
+        }
+    }
+}
